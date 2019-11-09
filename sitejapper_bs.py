e5b7b6c666454faa55fc4efbd7af54a8861bde53
Johannes Schneider <49132464+jojo20872@users.noreply.github.com>
2019-11-09 09:23:33 -0500

    Add comments to webscraping file

--- sitejapper_bs.py
+++ sitejapper_bs.py
@@ -3,42 +3,44 @@ import requests
 import csv
 import re
 
-f = open('C:/Users/jojo2/Documents/PythonProjects/Uber2/sitejabber_uber.csv', 'w')
+
+f = open('FILE DESTINATION', 'w')
 w = csv.writer(f)
 w.writerow(['Date', 'Stars', 'Comment'])
 
-
-
-
 pages = []
 
+# This appends all the pages for Sitejabber's Uber reviews to pages.
 for i in range(1, 33):
     url = 'https://www.sitejabber.com/reviews/uber.com?page=' + str(i) + '#reviews'
     pages.append(url)
 
 for page in pages:
+    # Gathering the html source code from the page
     source = requests.get(page).text
 
     soup = BeautifulSoup(source, 'lxml')
-
+    
+    # Find all reviews in the page
     reviews = soup.find_all('div', id=re.compile('^ReviewRow'))
 
     for review in reviews:
+        # Comment of review
         comment = review.find('div', id=re.compile('^ReviewText')).text
+        # Removing non-letters/formating
         comment = comment.replace("\t", "")
         comment = comment.replace("\n", "")
-        # print(comment)
-
+        
+        # Date of review
         date = review.find('div', class_='time tiny_text faded_text').text
+        # Removing non-letters/formating
         date = date.replace("\t", "")
         date = date.replace("\n", "")
-        # print(date)
 
+        # Star rating of review
         stars = review.find('div', id=re.compile('^ReviewText')).get('data-rating')
-        # print(stars)
 
-        # print()
-
+        # Writing to csv file
         w.writerow([date, stars, comment])
 
 f.close()
